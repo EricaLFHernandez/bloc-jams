7b930e6b72309b81a18d7a5c6d2cd0fd1c267c9d
Erica Hernandez <EricaLFHernandez@gmail.com>
2017-09-24 22:13:23 -0400

    Assignment 11 DOM Scripting: Album View complete

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,20 @@ var albumMarconi = {
     ]
 };
 
+var albumMiller = {
+    title: 'Obduction',
+    artist: 'Rand Miller',
+    label: 'Cyan Worlds',
+    year: '2016',
+    albumArtUrl: 'assets/images/album_covers/04.png',
+    songs: [
+        { title: 'Earth', duration: '3:31' },
+        { title: 'Kaptar', duration: '6:12' },
+        { title: 'Maray', duration: '2:39' },
+        { title: 'Soria', duration: '4:00' }
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
@@ -40,12 +54,14 @@ var createSongRow = function(songNumber, songName, songLength) {
       return template;
 };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+var albums = [albumPicasso,albumMarconi, albumMiller];
+
 var setCurrentAlbum = function(album) {
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -61,4 +77,13 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    var i = 1;
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[i]);
+        i++;
+        if (i == albums.length) {
+            i = 0;
+        }
+    });
 };
